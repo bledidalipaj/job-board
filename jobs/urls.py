50df400075a91a50a5ce23168a90cb25845a82e0
Bledi Dalipaj <bledikv@gmail.com>
2020-07-13 22:24:11 +0300

    Lay foundation for creating the post job page.

--- jobs/urls.py
+++ jobs/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [path("", views.list_jobs, name="list_jobs")]
+urlpatterns = [
+    path("", views.list_jobs, name="list_jobs"),
+    path("post_job/", views.post_job, name="post_job"),
+]
 
--- jobs/views.py
+++ jobs/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def list_jobs(request):
     return render(request, "jobs/list.html")
+
+
+def post_job(request):
+    return render(request, "jobs/post_job.html")
